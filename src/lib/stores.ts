5907ba78239de439c98e1c4ec510522d480f4eee
merger3 <merger3@duck.com>
2024-10-12 12:34:58 -0400

    drawing

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,6 +3,7 @@ import axios, { Axios, type AxiosInstance } from 'axios';
 import type { CamRequest, CamResponse } from '$types';
 export let token = writable<string>();
 export let server = writable<AxiosInstance>();
+export let drawing = writable<boolean>();
 
 export async function GetCam(r: CamRequest, a: AxiosInstance): Promise<CamResponse> {
 	let response = await a.post("/camera", {x: r.coordinates.x, y: r.coordinates.y, width: 0, height: 0, frameWidth: r.frameWidth, frameHeight: r.frameHeight, position: Number(r.position)});
