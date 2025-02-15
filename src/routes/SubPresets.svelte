e6543bb7b7f4902494d948f789d5fb9f9b267216
merger3 <johndoe@example.com>
2025-02-15 00:43:18 -0500

    Improved preset animation

--- src/routes/SubPresets.svelte
+++ src/routes/SubPresets.svelte
@@ -9,30 +9,39 @@
 	export let preset: ButtonPreset[];
 	export let quicksendSelected: string;
 
+	export let buttonWidth: string;
+
 	let s: number[] = [];
 
-    function buildCommand(preset: string, index: number) {
+    function buildCommand(preset: string, index: number, el: HTMLElement) {
+		// jQuery(el).css({"position": "absolute", "z-index": "1000"});
 		let newCommand: string = `!ptzload ${$camPresets.value} ${preset}`;
 		if (newCommand == $commandText) {
 			sendCommand({cmd: newCommand})
-			s[index] = 1.2;
+			s[index] = 1.3;
 		} else {
 			$commandText = newCommand;
 			if (quicksendSelected == "btn-outline-secondary") {
 				sendCommand({cmd: newCommand})
-				s[index] = 1.2;
+				s[index] = 1.3;
 			}
 			ClearStage($stage)
 		}
     }
+
+	function cleanUpAnimation(definition: any, i: number) {
+		s[i] = 1;
+		if (definition.scale == 1) {
+			// jQuery(scrollAreaElement).css("overflow", "auto");
+		}
+	}
 	
-	export let buttonWidth: string;
 </script>
 
 {#each preset as p, i}
 	{#if p.value != "separator"}
-		<Motion animate={{ scale: s[i] }} transition={{ duration: .13 }} onAnimationComplete={(definition) => {s[i] = 1}}  let:motion>
-			<button type="button" use:motion on:click={() => buildCommand(p.value, i)} class="btn btn-outline-warning btn-lg d-block px-0 mb-2 overflow-hidden position-relative h-16" style="width: {buttonWidth}">{p.value}</button>
+		<Motion animate={{ scale: s[i] }} transition={{ duration: .13 }} onAnimationComplete={(definition) => {cleanUpAnimation(definition, i)}}  let:motion>
+			<button type="button" use:motion on:click={(event) => buildCommand(p.value, i, event.currentTarget)} class="btn btn-outline-warning btn-lg d-block px-0 mb-2 overflow-hidden position-relative h-16" style="width: {buttonWidth}">{p.value}</button>
 		</Motion>
 	{/if}
 {/each}
